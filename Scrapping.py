1568499fa1d62bc6bb04444c9deddcbd20e53fd0
Shaun Ting <122302445+Shaun-Ting@users.noreply.github.com>
2023-02-04 14:04:43 -0500

    Add files via upload

---
+++ Scrapping.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+gpu = input("What product do you want to search for?")
+url = f"https://www.newegg.ca/p/pl?d={gpu}&N=4131"
+
+page = requests.get(url).text
+doc = BeautifulSoup(page,"html.parser")
+
+page_text = doc.find_all(class_="list-tool-pagination-text")
+pages = int(str(page_text).split("/")[1].split(">")[1][:-1])
+
+items_found = {}
+
+for page in range(1,pages + 1):
+    url = f"https://www.newegg.ca/p/pl?d={gpu}&N=4131&page={page}"
+    page = requests.get(url).text
+    doc = BeautifulSoup(page,"html.parser")
+
+    div = doc.find(class_="item-cells-wrap border-cells items-grid-view four-cells expulsion-one-cell")
+    items = div.find_all(text=re.compile(gpu))
+
+    for item in items:
+        parent = item.parent
+        if parent.name != "a":
+            continue
+        
+        link = parent['href']
+        pre_parent = item.find_parent(class_="item-container")
+
+        try:
+            price = pre_parent.find(class_="price-current").strong.string
+            items_found[item] = {"price": int(price.replace(",","")), "link": link}
+        except:
+            pass
+        # Sort dictionary - sort list - sort dictionary
+
+
+        
+sorted_items = sorted(items_found.items(), key=lambda x: x[1]['price'])
+
+for item in sorted_items:
+    print(item[0])
+    print(f"${item[1]['price']}")
+    print(item[1]['link'])
+    print("--------------")
+          
